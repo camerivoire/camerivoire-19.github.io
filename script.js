f99036d0d760551c28ba476434894ebc98f69f5d
Faxel Krypton <71553151+camerivoire@users.noreply.github.com>
2020-10-11 09:10:12 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,332 @@
+<script> var dataLayer = dataLayer || [];</script>
+	<script>
+		(function(x,y,z){(x[y]&&x[y](z))||z()})(window,'requestIdleCallback',function(){
+			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;
+					     j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					    })(window,document,'script','dataLayer','GTM-M8DN3N5');
+		});
+	</script>	
+		<script>		
+			(function(x,y,z){(x[y]&&x[y](z))||z()})(window,'requestIdleCallback',function(){			
+				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+					(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+					m=s.getElementsByTagName(o)[0];a.defer=1;a.src=g;m.parentNode.insertBefore(a,m)
+							})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+				ga('create', 'UA-121091809-1', 'auto');
+				ga('send', 'pageview');
+			});
+		</script>
+    
+    
+    <script type="application/ld+json" class="yoast-schema-graph">
+		{"@context":"https://schema.org","@graph":[{"@type":"WebSite","@id":"https://vpnpro.com/#website","url":"https://vpnpro.com/","name":"VPNpro","description":"VPN reviews, guides, trends and more","publisher":{"@id":"https://vpnpro.com/#organization"},"potentialAction":[{"@type":"SearchAction","target":"https://vpnpro.com/?s={search_term_string}","query-input":"required name=search_term_string"}],"inLanguage":"en-US"},{"@type":"ImageObject","@id":"https://vpnpro.com/best-vpn-services/#primaryimage","inLanguage":"en-US","url":"https://vpnpro.com/wp-content/uploads/Overall.jpg","width":800,"height":250,"caption":"devices with vpn on screens"},{"@type":"WebPage","@id":"https://vpnpro.com/best-vpn-services/#webpage","url":"https://vpnpro.com/best-vpn-services/","name":"The Best VPN Services for 2020 (We Tested 200+ VPNs)","isPartOf":{"@id":"https://vpnpro.com/#website"},"primaryImageOfPage":{"@id":"https://vpnpro.com/best-vpn-services/#primaryimage"},"datePublished":"2020-10-08","dateModified":"2020-10-08","description":"We've tested hundreds of VPNs and picked out the top 10 best VPN services for 2020 that scored the best in each and every category.","breadcrumb":{"@id":"https://vpnpro.com/best-vpn-services/#breadcrumb"},"inLanguage":"en-US","potentialAction":[{"@type":"ReadAction","target":["https://vpnpro.com/best-vpn-services/"]}]},{"@type":"BreadcrumbList","@id":"https://vpnpro.com/best-vpn-services/#breadcrumb","itemListElement":[{"@type":"ListItem","position":1,"item":{"@type":"WebPage","@id":"https://vpnpro.com/","url":"https://vpnpro.com/","name":"VPNpro"}},{"@type":"ListItem","position":2,"item":{"@type":"WebPage","@id":"https://vpnpro.com/best-vpn/","url":"https://vpnpro.com/best-vpn/","name":"Best VPN"}},{"@type":"ListItem","position":3,"item":{"@type":"WebPage","@id":"https://vpnpro.com/best-vpn-services/","url":"https://vpnpro.com/best-vpn-services/","name":"Best VPN Services for 2020"}}]}]}
+		</script>
+    
+    <noscript id="aonoscrcss">
+	        <link media="all" href="https://vpnpro.com/wp-content/cache/autoptimize/css/autoptimize_ad1af280be8bba77ed095e6a9563e7eb.css" rel="stylesheet" />
+		</noscript>
+    
+    
+    		<script> 
+													var gglcptch = {"options":{"version":"v2","sitekey":"","error":"<strong>Warning<\/strong>:&nbsp;More than one reCAPTCHA has been found in the current form. Please remove all unnecessary reCAPTCHA fields to make it work properly.","disable":0,"theme":"light"},"vars":{"visibility":false}};
+                  </script>
+									<script>
+										function initGoogleCaptcha() {			
+											var domSubmitButton = document.querySelector('#comments #submit');				
+											if ( typeof window.googleCaptchaWasInitialized === 'undefined' ) {					
+												if (domSubmitButton) {						
+													domSubmitButton.setAttribute('disabled', 'disabled');					
+												}					
+												var googleCaptchaScripts = JSON.parse('{"gglcptch_api":"https:\/\/www.google.com\/recaptcha\/api.js?render=explicit&#038;ver=1.58","gglcptch_script":"https:\/\/vpnpro.com\/wp-content\/plugins\/google-captcha\/js\/script.js?ver=1.58"}');
+												window.initGoogleCaptchaScript = function() {
+													loadScript(googleCaptchaScripts['gglcptch_script']);
+						if (domSubmitButton) {
+							domSubmitButton.removeAttribute('disabled');
+						}
+						window.disableComments = false;
+					};
+												var gCaptchaApiUrl = googleCaptchaScripts['gglcptch_api'].split('?');
+												if (gCaptchaApiUrl.length>1) {
+													gCaptchaApiUrl[1] = "onload=initGoogleCaptchaScript&" + gCaptchaApiUrl[1];
+												}
+												gCaptchaApiUrl = gCaptchaApiUrl.join('?');
+												loadScript(gCaptchaApiUrl);
+												window.googleCaptchaWasInitialized = true;		
+										}
+										function addCaptchaInitTrigger() {
+											if ('IntersectionObserver' in window) {
+												var observer = new IntersectionObserver(function(entries) {
+													if (entries[0].isIntersecting) initGoogleCaptcha();
+												}),
+							commentsForm = document.querySelector('#comments');
+					observer.observe(commentsForm);
+				} else {
+					initGoogleCaptcha();		
+			}
+										if( document.readyState === 'complete' ) {
+											addCaptchaInitTrigger();
+										} else {
+											document.addEventListener('DOMContentLoaded', function () {
+												addCaptchaInitTrigger();
+											});
+										}            
+    </script>             
+   <script defer src="https://vpnpro.com/wp-content/cache/autoptimize/js/autoptimize_13c5162f3bb22203aa79e215636ae9a8.js"></script>  
+   
+   									</script> 
+												<script defer="defer" src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script> 
+												<script>
+												var mdBoneVar = {"stickyHeader":{"toggle":"","hasAdminBar":false},"currentRelURI":"\/faxel-hackers\/","sliderOpts":{"autoplay":false,"timeout":5000}};
+												</script> 
+												
+												<script>
+												var ajaxUrl = 'https://faxkryp.blogspot.com/wp-admin/admin-ajax.php';
+			  var commentsStrings = {
+				errorMsgHeading: 'ERROR',
+				successMsgHeading: 'Thanks for your opinion!',
+				successMsgForAnonymous: 'Your comment will be checked for spam and approved as soon as possible.',
+				errorMsgSomeProblems: 'Some problems occured: ',
+				errorMsgErrAddingComment: 'Error while adding comment',
+				errorMsgServerTimeout: 'Error: Server doesn&#039;t respond.',
+				errorMsgRequiredFields: 'Please fill in all required fields!',
+				submitButtonTextLoading: 'Loading...',
+				submitButtonText: 'Post Comment',
+
+			};
+
+			if (document.readyState === 'complete') {
+				attachCommentAjaxHandler();
+			} else {
+				document.addEventListener('DOMContentLoaded', function() {
+					attachCommentAjaxHandler();
+				});
+			}
+
+			function resetGoogleCaptcha() {
+				if ( typeof grecaptcha != 'undefined' ) grecaptcha.reset();
+			}
+
+			function validateGoogleCaptcha() {
+				var passValidation = true;
+				if ( typeof grecaptcha != 'undefined' ) {
+					passValidation = grecaptcha.getResponse().length > 0;
+					passValidation ? jQuery( '.gglcptch' ).removeClass( 'error' ) : jQuery( '.gglcptch' ).addClass( 'error' );
+				}
+				return passValidation;
+			}
+
+			function validateCommentField() {
+				if ( typeof tinymce != 'undefined' && tinymce.editors.length > 0 && typeof tinymce.editors.comment != 'undefined' ) {
+					var commentText = tinymce.editors.comment.getContent( {
+						format: 'text'
+					} );
+					( commentText.length < 3 ) ? jQuery( '#comment' ).addClass( 'error' ): jQuery( '#comment' ).removeClass( 'error' );
+				} else {
+					jQuery( '#comment' ).validate();
+				}
+			}
+
+			function clearCommentField() {
+				if ( typeof tinymce != 'undefined' && tinymce.editors.length > 0 && typeof tinymce.editors.comment != 'undefined' ) tinymce.editors.comment.setContent( '' );
+				jQuery( '#comment' ).val( '' );
+			}
+
+			function showModal( heading, text ) {
+				heading = heading || '';
+				text = text || '';
+				jQuery( '[data-js-custom-modal-heading]' ).text( heading );
+				jQuery( '[data-js-custom-modal-text]' ).text( text );
+				jQuery( '[data-js-custom-modal]' ).toggleClass( 'opened' );
+			}
+
+			function updateCommentsTitle( newCommentsTitle ) {
+				if ( typeof newCommentsTitle != 'undefined' && newCommentsTitle.length > 0 ) jQuery( "[data-js-comments-title]" ).replaceWith( newCommentsTitle );
+			}
+
+			function attachCommentAjaxHandler() {
+				jQuery.extend( jQuery.fn, {
+					/*
+					* check if field value length more than 3 symbols ( for name and comment )
+					*/
+					validate: function() {
+						if ( jQuery( this ).val().length < 2 ) {
+							jQuery( this ).addClass( 'error' );
+							return false;
+						} else {
+							jQuery( this ).removeClass( 'error' );
+							return true;
+						}
+					},
+					/*
+					* check if email is correct
+					* add to your CSS the styles of .error field, for example border-color:red;
+					*/
+					validateEmail: function() {
+						var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+							emailToValidate = jQuery( this ).val();
+						if ( !emailReg.test( emailToValidate ) || emailToValidate == "" ) {
+							jQuery( this ).addClass( 'error' );
+							return false;
+						} else {
+							jQuery( this ).removeClass( 'error' );
+							return true;
+						}
+					},
+				} );
+				jQuery( function( $ ) {
+					$( 'body' ).on( 'click', '[data-js-custom-modal]', function( e ) {
+						$( this ).toggleClass( 'opened' );
+					} );
+					$( 'body' ).on( 'submit', '#commentform', function( e ) {
+						e.preventDefault();
+						if (typeof window.disableComments !== 'undefined' && window.disableComments) {
+							return false;
+						}
+						var button = $( '#submit' ), // submit button
+							respond = $( '#respond' ), // comment form container
+							commentlist = $( '.comment-list' ), // comment list container
+							cancelReplyLink = $( '#cancel-comment-reply-link' );
+						var parser = new DOMParser();
+						if ( $( '#author' ).length ) $( '#author' ).validate();
+						if ( $( '#email' ).length ) $( '#email' ).validateEmail();
+						validateCommentField();
+												
+						var passValidation = validateGoogleCaptcha();
+						// if comment form isn't in process, submit it
+						if ( !button.hasClass( 'loadingForm' ) && !$( '#author' ).hasClass( 'error' ) && !$( '#email' ).hasClass( 'error' ) && !$( '#comment' ).hasClass( 'error' ) && passValidation ) {
+							$.ajax( {
+								type: 'POST',
+								url: ajaxUrl, // admin-ajax.php URL
+								data: $( this ).serialize() + '&action=ajaxcomments', // send form data + action parameter
+								beforeSend: function( xhr ) {
+									// what to do just after the form has been submitted
+									button
+										.addClass( 'loadingForm' )
+										.attr( 'disabled', 'disabled' )
+										.html( commentsStrings.submitButtonTextLoading );
+								},
+								error: function( request, status, error ) {
+									if ( status == 500 ) {
+										showModal( commentsStrings.errorMsgHeading, commentsStrings.errorMsgErrAddingComment );
+									} else if ( status == 'timeout' ) {
+										showModal( commentsStrings.errorMsgHeading, commentsStrings.errorMsgServerTimeout );
+									} else {
+										// output WordPress errors
+										var errorBody = parser.parseFromString( request.responseText, "text/html" );
+										var errorString = $( errorBody ).find( "p" ).first().text();
+										showModal( commentsStrings.errorMsgHeading, errorString );
+									}
+								},
+								success: function( responseHTML ) {
+									var responseDOM = parser.parseFromString( responseHTML, 'text/html' );
+									var newComment = $( responseDOM ).find( 'li.comment' );
+									var newCommentsTitle = $( responseDOM ).find( '[data-js-comments-title]' );
+									var newCommentHTML = newComment.length ? newComment[ 0 ].outerHTML : '';
+									if ( newComment.length ) {
+										// if this post already has comments
+										if ( commentlist.length > 0 ) {
+											// if in reply to another comment
+											if ( respond.parent().hasClass( 'comment' ) ) {
+												// if the other replies exist
+												if ( respond.parent().children( '.children' ).length ) {
+													respond.parent().children( '.children' ).append( newCommentHTML );
+												} else {
+													// if no replies, wrap in <ol class="children">
+													newCommentHTML = '<ol class="children">' + newCommentHTML + '</ol>';
+													respond.parent().append( newCommentHTML );
+												}
+												// close respond form
+												cancelReplyLink.trigger( 'click' );
+											} else {
+												commentlist.append( newCommentHTML );
+											}
+										} else {
+											newCommentHTML = '<ol class="comment-list">' + newCommentHTML + '</ol>';
+											respond.before( $( newCommentHTML ) );
+										}
+										clearCommentField();
+										if ( newCommentsTitle.length ) updateCommentsTitle( newCommentsTitle );
+										var heading = commentsStrings.successMsgHeading;
+										var text = newComment.find( ".comment-awaiting-moderation" ).length ? commentsStrings.successMsgForAnonymous : '';
+										showModal( commentsStrings.successMsgHeading, text );
+									} else {
+										showModal( commentsStrings.errorMsgHeading, commentsStrings.errorMsgSomeProblems + newCommentHTML );
+									}
+								},
+								complete: function( response ) {
+									button
+										.removeClass( 'loadingForm' )
+										.removeAttr( 'disabled' )
+										.html( commentsStrings.submitButtonText );
+									resetGoogleCaptcha();
+								}
+							} );
+						} else {
+							showModal( commentsStrings.errorMsgHeading, commentsStrings.errorMsgRequiredFields );
+						}
+						return false;
+					} );
+				} );
+			}
+												</script> 
+                        
+                    
+                        
+                        
+                        
+                        <script>
+												 window.openPopupUrls = window.openPopupUrls || {};
+                         window.openPopupUrls.sfb = 'https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffaxkryp.blogspot.com';
+                         window.openPopupUrls.stw = 'https://twitter.com/intent/tweet?text=Le meilleur;url=https%3A%2F%2Ffaxkryp.blogspot.com;via=Faxel';
+                         window.openPopupUrls.sfbm = 'https://www.addtoany.com/add_to/facebook_messenger?linkurl=https%3A%2F%2Ffaxkryp.blogspot.com;linkname=Le meilleur des bloggers';
+                         window.openPopupUrls.srd = 'http://www.reddit.com/submit?url=https%3A%2F%2Ffaxkryp.blogspot.com;title= Le meilleur';
+                         window.openPopupUrls.slk = 'https://www.linkedin.com/shareArticle?url=https%3A%2F%2Ffaxkryp.blogspot.com;title=Le meilleur';
+												</script>
+			
+      <script>
+	var ajaxurl = 'https://vpnpro.com/wp-admin/admin-ajax.php',
+					post_id = '1274',
+					cpage = 1,
+					cPagesCount = 2;
+
+				function handleShowCommentsClick () {
+					var domCommentsList       = $('[data-js-comments-list]');
+					var domShowCommentsAction = $('[data-js-show-comments]');
+					var domShowCommentsButton = $('[data-js-show-comments-button]');
+
+					cpage++;
+
+					$.ajax({
+						url: ajaxurl,
+						data: {
+							'action': 'cloadmore',
+							'post_id': post_id,
+							'cpage' : cpage,
+						},
+						type: 'POST',
+						beforeSend: function ( xhr ) {
+							domShowCommentsButton.attr('disabled', true);
+							domShowCommentsButton.text('Loading...');
+						},
+						success: function( data ){
+							if( data ) {
+								domCommentsList.append( data );
+
+								if ( cpage == cPagesCount ) {
+									domShowCommentsAction.remove();
+								} else {
+									domShowCommentsButton.removeAttr('disabled');
+									domShowCommentsButton.text('Show more comments');
+								}
+							} else {
+								domShowCommentsAction.remove();
+							}
+						},
+						error: function () {
+							domShowCommentsAction.remove();
+						}
+					});
+				};
+												</script> 
